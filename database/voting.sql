b5b6b7d89f09ab320bce2859cb0953e1224dc02a
abenaewudzie <123464391+abenaewudzie@users.noreply.github.com>
2024-08-29 16:12:51 +0000

    Update voting.sql

--- database/voting.sql
+++ database/voting.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2018 at 03:28 AM
--- Server version: 10.1.24-MariaDB
--- PHP Version: 7.1.6
+-- Generation Time: Aug 29, 2024 at 05:31 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -36,18 +35,23 @@ CREATE TABLE `candidate` (
   `year_level` varchar(100) NOT NULL,
   `gender` varchar(100) NOT NULL,
   `img` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `candidate`
 --
 
 INSERT INTO `candidate` (`candidate_id`, `position`, `firstname`, `lastname`, `year_level`, `gender`, `img`) VALUES
-(1, 'President', 'Calvin', 'Ampadu', '4th Year', 'Male', 'upload/2017-05-23 08_58_47-17+-+1 (480Ã—586).png');
-INSERT INTO `candidate` (`candidate_id`, `position`, `firstname`, `lastname`, `year_level`, `gender`, `img`) VALUES
-(1, 'President', 'Kelvin', 'Asare', '3rd Year', 'Male', 'upload/2017-05-23 08_58_47-17+-+1 (480Ã—586).png');
-INSERT INTO `candidate` (`candidate_id`, `position`, `firstname`, `lastname`, `year_level`, `gender`, `img`) VALUES
-(1, 'President', 'Elizabeth', 'Sey', '4th Year', 'Male', 'upload/2017-05-23 08_58_47-17+-+1 (480Ã—586).png');
+(1, 'President', 'Calvin', 'Ampadu', '4th Year', 'Male', 'upload/images (2).jpeg'),
+(2, 'President', 'Elizabeth', 'Sey', '3rd year', 'Female', 'upload/download (4).jpeg'),
+(3, 'President', 'Kelvin', 'Asare', '3rd year', 'Male', 'upload/images (3).jpeg'),
+(4, 'Vice President', 'Nana Kwesi', 'Annim', '4th year', 'Male', 'upload/download (7).jpeg'),
+(5, 'Vice President', 'Sandra', 'Ayeku', '2nd Year', 'Female', 'upload/download (6).jpeg'),
+(6, 'General Secretary', 'Helcy', 'Boadu', '2nd year', 'Female', 'upload/download (5).jpeg'),
+(7, 'Financial Secretary', 'Rockson', 'Amable', '3rd Year', 'Male', 'upload/download (10).jpeg'),
+(8, 'Financial Secretary', 'Samuella', 'Agaana', '5th Year', 'Female', 'upload/download (8).jpeg'),
+(9, 'General Secretary', 'Cephas', 'Gbemumu', '3rd Year', 'Male', 'upload/images (8).jpeg'),
+(10, 'Treasurer', 'Eugene', 'Debrah ', '2nd Year', 'Male', 'upload/Glasses-for-Oval-shape.webp');
 
 -- --------------------------------------------------------
 
@@ -61,14 +65,14 @@ CREATE TABLE `user` (
   `password` varchar(100) NOT NULL,
   `firstname` varchar(100) NOT NULL,
   `lastname` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `firstname`, `lastname`) VALUES
-(1, 'admin', 'admin', 'Priscilla ','Ewudzie' 'Sam');
+(1, 'admin', 'admin', 'Priscilla Ewudzie', 'Sam');
 
 -- --------------------------------------------------------
 
@@ -85,14 +89,18 @@ CREATE TABLE `voters` (
   `year_level` varchar(100) NOT NULL,
   `status` varchar(100) NOT NULL,
   `account` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `voters`
 --
 
 INSERT INTO `voters` (`voters_id`, `id_number`, `password`, `firstname`, `lastname`, `year_level`, `status`, `account`) VALUES
-(1, 21241523, '', 'Lois', 'Agyapomaa', '3rd Year', 'Unvoted', '');
+(1, 21241523, 'EjHC9g9x', 'Lois', 'Agyampomaa', '3rd Year', 'Voted', 'Active'),
+(3, 21241526, 'qXhErS7Z', 'Sarah', 'Boadu', '2nd year', 'Unvoted', 'Active'),
+(2, 21241524, 'r8s3qSTD', 'Sakura', 'Haruna', '3rd Year', 'Unvoted', 'Active'),
+(4, 21241527, 'h2LEPAk1', 'Winfred', 'Agyekum', '4th year', 'Unvoted', 'Active'),
+(5, 21241528, 'Yl5ZYeF8', 'Wilhelmina', 'Kumkum', '1st year', 'Unvoted', 'Active');
 
 -- --------------------------------------------------------
 
@@ -104,7 +112,21 @@ CREATE TABLE `votes` (
   `vote_id` int(255) NOT NULL,
   `candidate_id` varchar(255) NOT NULL,
   `voters_id` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`vote_id`, `candidate_id`, `voters_id`) VALUES
+(44, '1', '1'),
+(45, '5', '1'),
+(46, '6', '1'),
+(47, '7', '1'),
+(48, '10', '1'),
+(49, '', '1'),
+(50, '', '1'),
+(51, '', '1');
 
 --
 -- Indexes for dumped tables
@@ -142,22 +164,26 @@ ALTER TABLE `votes`
 -- AUTO_INCREMENT for table `candidate`
 --
 ALTER TABLE `candidate`
-  MODIFY `candidate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `candidate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `voters`
 --
 ALTER TABLE `voters`
-  MODIFY `voters_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `voters_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `vote_id` int(255) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `vote_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
