80aa331888d42372a37fbd39bba88411813d675e
abenaewudzie <123464391+abenaewudzie@users.noreply.github.com>
2024-09-02 09:30:01 +0000

    Update dbcon.php

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('sql308.infinityfree.com', 'if0_37219291', 'FuyWAjsvKhTRqzP', 'if0_37219291_voting');
+	$conn = new mysqli('localhost', 'root', '', 'voting');
 	
 	if(!$conn){
 		die("Error: Failed to connect to database");
